83f5365920b1ec1ceefff5f70e2ef99086461973
Guilherme Brito <guilhermebritto.prof@gmail.com>
2016-06-25 18:18:29 -0300

    Corrigindo Bootstrap
    Correção

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="utf-8">
+	<title> Intranet Linkfort </title>
+	<!-- Incluindo o CSS do Bootstrap -->
+	<link href="framework/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+</head>
+<body>
+
+<!-- Incluindo o jQuery que é requisito do JavaScript do Bootstrap -->
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+
+<!-- Incluindo o JavaScript do Bootstrap -->
+<script src="framework/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
