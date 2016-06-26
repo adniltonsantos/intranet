cdc7512f33386f5adc1d7dab15304edfd6a8bdc4
Adnilton <adniltonweb@gmail.com>
2016-06-26 00:17:27 -0300

    top para topo

---
+++ topo.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <title> Intranet Linkfort </title>
+  <?php include "framework.php"; ?>
+  
+</head>
+<body>
+
+<?php include "menu.php" ?>
+Apenas uma modificação teste
