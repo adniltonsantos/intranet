e32b40427331d27c822c7255687766fa093e7d72
Adnilton Santos <adniltonweb@gmail.com>
2016-06-26 18:24:44 -0300

    Form de Login e Senha centralizado para todas resolução de tela.

--- login.php
+++ login.php
@@ -1,72 +1,38 @@
-<<<<<<< HEAD
+
 <!DOCTYPE html>
 <head>
 	<meta charset="utf-8">
 	<title> Intranet Linkfort 10</title>
-=======
-<<<<<<< HEAD
-<!DOCTYPE html>
-<head>
-	<meta charset="utf-8">
-	<title> Intranet Linkfort 10</title>
+
 	<?php include "framework.php"; ?>
-	
+
 </head>
 <body>
-  <div id="bloco">
-	<div id="divCenterImg">
-		<img src="img/logo_linkfort.png" height="100px" width="300px">
-    </div>
-    <div id="divCenter">
+<div class="bloco">
+
+<center>
+<img src="img/logo_linkfort.png" width="250px" height="100px">
+</br>
+</br>
+</center>
+
+   <div id="divCenter">
         <form id="frmPost"  method="post" action="home.php">
            <div class="mdl-textfield mdl-js-textfield">
-    			<input class="mdl-textfield__input" type="text" id="sample1">
-   				<label class="mdl-textfield__label" for="sample1">Login...</label>
-  			</div>
-  			<div class="mdl-textfield mdl-js-textfield">
-    			<input class="mdl-textfield__input" type="password" id="sample1">
-   				<label class="mdl-textfield__label" for="sample1">Senha...</label>
-  			</div>
-  			<button type="submit" class="button buttonBlue">Entrar
-    			<div class="ripples buttonRipples"><span class="ripplesCircle"></span></div>
- 			 </button>
+          <input class="mdl-textfield__input" type="text" id="sample1">
+          <label class="mdl-textfield__label" for="sample1">Login...</label>
+        </div>
+        <div class="mdl-textfield mdl-js-textfield">
+          <input class="mdl-textfield__input" type="password" id="sample1">
+          <label class="mdl-textfield__label" for="sample1">Senha...</label>
+        </div>
+        <button type="submit" class="button buttonBlue">Entrar
+          <div class="ripples buttonRipples"><span class="ripplesCircle"></span></div>
+       </button>
         </form>
-    </div>
-  </div>
-</body>
-=======
-<!DOCTYPE html>
-<head>
-	<meta charset="utf-8">
-	<title> Intranet Linkfort </title>
->>>>>>> 88678b7e0e0c66444922c2c86fae91b93d1397af
-	<?php include "framework.php"; ?>
-	
-</head>
-<body>
-  <div id="bloco">
-	<div id="divCenterImg">
-		<img src="img/logo_linkfort.png" height="100px" width="300px">
-    </div>
-    <div id="divCenter">
-        <form id="frmPost"  method="post" action="home.php">
-           <div class="mdl-textfield mdl-js-textfield">
-    			<input class="mdl-textfield__input" type="text" id="sample1">
-   				<label class="mdl-textfield__label" for="sample1">Login...</label>
-  			</div>
-  			<div class="mdl-textfield mdl-js-textfield">
-    			<input class="mdl-textfield__input" type="password" id="sample1">
-   				<label class="mdl-textfield__label" for="sample1">Senha...</label>
-  			</div>
-  			<button type="submit" class="button buttonBlue">Entrar
-    			<div class="ripples buttonRipples"><span class="ripplesCircle"></span></div>
- 			 </button>
-        </form>
-    </div>
-  </div>
-</body>
-<<<<<<< HEAD
-=======
->>>>>>> ajustando_tela_login_centralizado
->>>>>>> 88678b7e0e0c66444922c2c86fae91b93d1397af
-</html>
+      </div>
+
+
+
+
+ </div>
